2a5f85c9d8d7676bb82f7f6e3b3b92760d4336c7
Ericson <luckaeric100@gmail.com>
2020-06-19 22:36:03 +0300

    My main commit for my first deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FontAwesome } from './font-awesome';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'faIconLists';
+  result_is_empty:boolean=false;
+  fa:any=this.fontAwesome.list.icons;
+  listes:string[]=[];
+  formSearch:any;
+  constructor(private fontAwesome : FontAwesome){}
+
+  ngOnInit(){
+    this.formSearchControl();
+    this.searchStringInArray('',this.fa);
+    // console.log(this.listes);
+    // console.log(this.searchStringInArray('justify',this.listes));
+    
+  }
+
+  public formSearchControl() {
+    this.formSearch = new FormGroup({
+      search_field : new FormControl('')
+    });
+  }
+
+  searchStringInArray(str:any,strArray:string[]){
+    let result:string[]=[];
+    for (let j = 0; j < strArray.length; j++) {
+      if (strArray[j].match(str.search_field)) {
+        result.push(strArray[j]);
+      }
+    }
+    (result.length==0)?this.result_is_empty=true:this.result_is_empty=false;
+    this.listes = result;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesome } from './font-awesome';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    FontAwesome
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
